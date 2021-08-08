e8382e289623456bb9c4912adf0f29ee3e950581
Mamoun Hussein <mamoon.husen1006@gmail.com>
2021-08-08 20:36:16 +0300

    fineshed lab6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style/style.sass";
+import Main from "./components/main";
+import Footer from "./components/footer";
+import Header from "./components/header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import axios from "axios";
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <Main />
+        <Footer />
+      </>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/main.js
@@ -0,0 +1,131 @@
+import React, { Component } from "react";
+import axios from "axios";
+import {
+  InputGroup,
+  Button,
+  Container,
+  Row,
+  Col,
+  FormControl,
+  Card,
+  Badge,
+  Alert,
+  Spinner,
+} from "react-bootstrap";
+import dotenv from "dotenv";
+dotenv.config();
+const myKey = process.env.REACT_APP_myKey;
+export default class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      location: "",
+      lon: "",
+      lat: "",
+      displayName: "",
+      src: "",
+      err: false,
+    };
+  }
+  handleLocation = (e) => {
+    this.setState({
+      loading: true,
+    });
+    axios
+      .get(
+        `https://eu1.locationiq.com/v1/search.php?key=${myKey}&q=${this.state.location}&format=json`
+      )
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          err: false,
+          lon: res.data[0].lon,
+          lat: res.data[0].lat,
+          displayName: res.data[0].display_name,
+          src: `https://maps.locationiq.com/v3/staticmap?key=${myKey}&center=${res.data[0].lat},${res.data[0].lon}&zoom=16&size=600x600&markers=icon:large-red-cutout|${res.data[0].lat},${res.data[0].lon}|${res.data[0].lat},${res.data[0].lon}`,
+        });
+      })
+      .catch((err) => {
+        this.setState({
+          err: true,
+          lon: "",
+          lat: "",
+          displayName: "",
+          src: "",
+        });
+      })
+      .then(() => {
+        this.setState({
+          loading: false,
+        });
+      });
+  };
+
+  render() {
+    return (
+      <main>
+        <Container style={{ width: "30rem" }}>
+          {this.state.err && (
+            <Alert variant="danger"> No location in this name</Alert>
+          )}
+          <Row ls={12} xs={12}>
+            <InputGroup
+              className="mb-3"
+              onChange={(e) => {
+                this.setState({
+                  // @ts-ignore
+                  location: e.target.value,
+                });
+              }}
+            >
+              <FormControl
+                placeholder="City Name"
+                aria-label="City Name"
+                id="location"
+              />
+              <Button
+                variant="outline-secondary"
+                id="button-addon2"
+                onClick={this.handleLocation}
+              >
+                Explore!
+              </Button>
+            </InputGroup>
+          </Row>
+          {this.state.displayName && (
+            <Row>
+              <Col>
+                <Card>
+                  <Row>
+                    {this.state.loading ? (
+                      <Spinner
+                        animation="border"
+                        role="status"
+                        variant="primary"
+                      />
+                    ) : (
+                      <Card.Img src={this.state.src} />
+                    )}
+                  </Row>
+                  <Card.Body>
+                    <Card.Title>
+                      {this.state.displayName}
+                      <Badge bg="secondary">City Name</Badge>
+                    </Card.Title>
+                    <Card.Text>
+                      {this.state.lon} <Badge bg="secondary">Longitude</Badge>
+                    </Card.Text>
+                    <Card.Text>
+                      {this.state.lat} <Badge bg="secondary">Latitude</Badge>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          )}
+        </Container>
+      </main>
+    );
+  }
+}
