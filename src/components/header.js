ef4232ba712c65f30084c0c9e6d3e7af3a196c4d
Mamoun Hussein <mamoon.husen1006@gmail.com>
2021-08-08 21:18:58 +0300

    fixed bug in env

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "../assets/logo.png";
 
 export default class Header extends Component {
   render() {
--- src/components/main.js
+++ src/components/main.js
@@ -12,9 +12,9 @@ import {
   Alert,
   Spinner,
 } from "react-bootstrap";
-import dotenv from "dotenv";
-dotenv.config();
-const myKey = process.env.REACT_APP_myKey;
+// import dotenv from "dotenv";
+// dotenv.config();
+// const myKey = process.env.REACT_APP_myKey;
 export default class Main extends Component {
   constructor() {
     super();
@@ -34,7 +34,7 @@ export default class Main extends Component {
     });
     axios
       .get(
-        `https://eu1.locationiq.com/v1/search.php?key=${myKey}&q=${this.state.location}&format=json`
+        `https://eu1.locationiq.com/v1/search.php?key=pk.4c2df93362745f52672fbdcb21fb9301&q=${this.state.location}&format=json`
       )
       .then((res) => {
         console.log(res);
@@ -43,7 +43,7 @@ export default class Main extends Component {
           lon: res.data[0].lon,
           lat: res.data[0].lat,
           displayName: res.data[0].display_name,
-          src: `https://maps.locationiq.com/v3/staticmap?key=${myKey}&center=${res.data[0].lat},${res.data[0].lon}&zoom=16&size=600x600&markers=icon:large-red-cutout|${res.data[0].lat},${res.data[0].lon}|${res.data[0].lat},${res.data[0].lon}`,
+          src: `https://maps.locationiq.com/v3/staticmap?key=pk.4c2df93362745f52672fbdcb21fb9301&center=${res.data[0].lat},${res.data[0].lon}&zoom=16&size=600x600&markers=icon:large-red-cutout|${res.data[0].lat},${res.data[0].lon}|${res.data[0].lat},${res.data[0].lon}`,
         });
       })
       .catch((err) => {
@@ -69,7 +69,7 @@ export default class Main extends Component {
           {this.state.err && (
             <Alert variant="danger"> No location in this name</Alert>
           )}
-          <Row ls={12} xs={12}>
+          <Row lg={12} xs={12}>
             <InputGroup
               className="mb-3"
               onChange={(e) => {
